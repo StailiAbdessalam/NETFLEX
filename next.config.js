ed5bef3b187d1a72b021564bedc5337e1f2d498d
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-18 12:21:35 +0100

    config the image nex

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["rb.gy", "image.tmdb.org"],
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
