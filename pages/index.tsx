23a41eb9f25974a617c20bedda9947a7e06d81db
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-18 12:23:23 +0100

    index or page home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../Components/Banner";
 import Header from "../Components/Header";
 import { Movie } from "../typing";
 import requests from "../utils/requestq";
@@ -31,6 +32,22 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
+        <Banner netflixOriginals={netflixOriginals} />
+
+        <section className="md:space-y-24">
+          {/* <Row title="Trending Now" movies={trendingNow} /> */}
+          {/* <Row title="Top Rated" movies={topRated} /> */}
+          {/* <Row title="Action Thrillers" movies={actionMovies} /> */}
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          {/* <Row title="Comedies" movies={comedyMovies} /> */}
+          {/* <Row title="Scary Movies" movies={horrorMovies} /> */}
+          {/* <Row title="Romance Movies" movies={romanceMovies} /> */}
+          {/* <Row title="Documentaries" movies={documentaries} /> */}
+        </section>
+      </main>
     </div>
   );
 };
