82da91fd8cd1e064cdfeec40d8633c7d0323af52
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-19 11:59:59 +0100

    modal components

---
+++ Components/Modal.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import MuiModel from "@mui/material/Modal";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
+import {
+  CheckIcon,
+  PlusIcon,
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { FaPlay } from "react-icons/fa";
+import { Element } from "../typing";
+
+const Modal = () => {
+  //   const showModel = useRecoilValue(modalState);
+  const [showModel, setShowModel] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState<string>("");
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (!movie) return;
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === "tv" ? "tv" : "movie"
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      )
+        .then((response) => response.json())
+        .catch((err) => console.log(err));
+
+      if (data.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+      }
+    }
+    fetchMovie();
+  }, [movie]);
+
+  const handlClose = () => {
+    setShowModel(false);
+  };
+  return (
+    <MuiModel open={showModel} onClose={handlClose}>
+      <>
+        {/* <Toaster position="bottom-center" /> */}
+        <button
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          onClick={handlClose}
+        >
+          <XIcon className="h-6 w-6" />
+        </button>
+
+        <div className="relative pt-[56.25%]">
+          {/* <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0", left: "0" }}
+            playing
+            muted={muted}
+          /> */}
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+              {/* <button className="modalButton" onClick={handleList}>
+                {addedToList ? (
+                  <CheckIcon className="h-7 w-7" />
+                ) : (
+                  <PlusIcon className="h-7 w-7" />
+                )}
+              </button> */}
+              <button className="modalButton">
+                <ThumbUpIcon className="h-6 w-6" />
+              </button>
+            </div>
+            {/* <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
+            </button> */}
+          </div>
+        </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres:</span>{" "}
+                  {/* {genres.map((genre) => genre.name).join(", ")} */}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language:</span>{" "}
+                  {movie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes:</span>{" "}
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    </MuiModel>
+  );
+};
+
+export default Modal;
