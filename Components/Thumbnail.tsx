708b72bd83398aab04ede5dce5ab7d642fafcfd6
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-19 12:01:23 +0100

    Thumbnail components and send movie to modal componens

--- Components/Thumbnail.tsx
+++ Components/Thumbnail.tsx
@@ -1,7 +1,7 @@
-// import { DocumentData } from 'firebase/firestore'
+import { DocumentData } from "firebase/firestore";
 import Image from "next/image";
-// import { useRecoilState } from 'recoil'
-// import { modalState, movieState } from '../atoms/modalAtom.'
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
 import { Movie } from "../typing";
 
 interface Props {
@@ -9,16 +9,16 @@ interface Props {
 }
 
 function Thumbnail({ movie }: Props) {
-  //   const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
-  //   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+  const [showModal, setShowModal] = useRecoilState(modalState);
 
   return (
     <div
       className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
-      //   onClick={() => {
-      //     setCurrentMovie(movie)
-      //     setShowModal(true)
-      //   }}
+      onClick={() => {
+        setCurrentMovie(movie);
+        setShowModal(true);
+      }}
     >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
